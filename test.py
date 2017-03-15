bd3b017c5509b596a8173b0da318b6af8e17e278
wasimAlmadfaa <wasim.almadfaa1997@gmail.com>
2017-03-15 07:09:03 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+print ("number \t square ")
+print ("-----------------")
+for number in range(0,11):
+    if number == 0:
+        continue
+    square = number ** 2
+    print (number,"\t",square)
+    
+
